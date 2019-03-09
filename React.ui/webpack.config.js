a153a348fb7b7a57a5517d664dacbd57c59992c5
ryan leslie <ryanleslie33@aol.com>
2019-03-08 16:33:40 -0800

    Add project files.

---
+++ React.ui/webpack.config.js
@@ -0,0 +1,21 @@
+﻿"use strict";
+
+module.exports = {
+    entry: "./index.js",
+    output: {
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: "babel-loader",
+                exclude: /node_modules/,
+                query: {
+                    presets: ["es2015", "react"]
+                }
+            }
+        ]
+    }
+
+};
